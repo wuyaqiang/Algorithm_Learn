2329437814f7aad84f7599e315cdfa905b0a3141
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-15 22:23:18 +0800

    add jianzhi 53

--- 剑指Offer/MyCode/No_53.py
+++ 剑指Offer/MyCode/No_53.py
@@ -50,17 +50,53 @@ def count_of_number(nums, k):
         return last_idx - first_idx + 1
     return 0
 
-def get_missing_number(nums, length):
+def get_missing_number(nums):
     '''
     0到n-1中缺失的数字
     '''
+    # 解法一：利用求和公式，时间复杂度为O(n)
+    # length = len(nums)
+    # return length * (length + 1) // 2 - sum(nums)
 
+    # 解法二：结合数组递增特性，利用二分查找算法，时间复杂度为O(logn)
+    length = len(nums)
+    if not nums or length == 0:
+        return -1
+    l, r = 0, length - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == mid:
+            l = mid + 1
+        else:
+            if mid > 0 and nums[mid - 1] == mid - 1:
+                return mid
+            r = mid - 1
+    if l == length:
+        return length
+    return -1
 
+def get_number_same_as_index(nums):
+    '''
+    数组中数值和下标相等的元素
+    '''
+    if not nums or len(nums) == 0:
+        return -1
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == mid:
+            return mid
+        elif nums[mid] < mid:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
 
 if __name__ == '__main__':
-    print(count_of_number([1,2,3,3,3,3,4,5], 3))
 
-
+    # print(count_of_number([1,2,3,3,3,3,4,5], 3))
+    # print(get_missing_number([0,1,2,4,5]))
+    print(get_number_same_as_index([-3, -1, 1, 3, 5]))
 
 
 
