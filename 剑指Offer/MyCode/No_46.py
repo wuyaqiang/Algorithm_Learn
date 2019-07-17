6ec576d110dc134b9ff1b434179e37c54ce01c07
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-17 21:18:42 +0800

    add jianzhi 46

---
+++ 剑指Offer/MyCode/No_46.py
@@ -0,0 +1,28 @@
+'''
+剑指 Offer 第46题 把数字翻译成字符串
+'''
+
+def trans_int_to_string(num):
+    if num < 0:
+        return ""
+    num = str(num)
+    length = len(num)
+    dp = [0] * length
+    for i in range(length - 1, -1, -1):
+        count = 0
+        if i == length - 1:
+            count += 1
+        else:
+            count += dp[i + 1]
+            if 10 <= int(num[i] + num[i + 1]) <= 25:
+                if i < length - 2:
+                    count += dp[i + 2]
+                else:
+                    count += 1
+        dp[i] = count
+
+    return dp[0]
+
+if __name__ == '__main__':
+
+    print(trans_int_to_string(11))
