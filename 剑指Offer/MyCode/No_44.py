dd53f0f79c3349aca98ec847750ab71dce2817e5
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-18 17:13:47 +0800

    add jianzhi 44

--- 剑指Offer/MyCode/No_44.py
+++ 剑指Offer/MyCode/No_44.py
@@ -3,3 +3,19 @@
 leetcode 400. Nth Digit
 '''
 
+def find_nth_digit(n):
+    if n < 0:
+        return -1
+    digits = 1
+    while True:
+        numbers = 9 * pow(10, digits - 1)
+        if n < digits * numbers:
+            num = pow(10, digits - 1) + (n - 1) // digits
+            return int(str(num)[n % digits - 1])
+
+        n -= digits * numbers
+        digits += 1
+
+if __name__ == '__main__':
+
+    print(find_nth_digit(19))
