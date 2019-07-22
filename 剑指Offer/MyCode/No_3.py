13305d83293874e7ca8265e8c6003072c0344c4a
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-22 11:20:00 +0800

    add jianzhi 3

---
+++ 剑指Offer/MyCode/No_3.py
@@ -0,0 +1,50 @@
+'''
+剑指 Offer 第3题
+leetcode 287. Find the Duplicate Number
+'''
+
+def findDuplicate(nums):
+    '''
+    数组中重复的数字
+    '''
+
+    # 解法一：修改了原始数组，时间复杂度为O(n)
+    # i = 0
+    # while i < len(nums):
+    #     if nums[i] == i + 1:
+    #         i += 1
+    #     else:
+    #         if nums[nums[i] - 1] == nums[i]:
+    #             return nums[i]
+    #         nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+    # return -1
+
+    # 解法二：利用二分查找，不修改原始数组，时间复杂度为O(nlogn)
+    # low, high = 1, len(nums) - 1
+    # while low <= high:
+    #     mid = low + (high - low) // 2
+    #     count = 0
+    #     for n in nums:
+    #         if low <= n <= mid:
+    #             count += 1
+    #     if low == high:
+    #         if count > 1:
+    #             return low
+    #         else:
+    #             break
+    #     if count > mid - low + 1:
+    #         high = mid
+    #     else:
+    #         low = mid + 1
+    # return -1
+
+    # 解法三：利用快慢指针，不修改原始数组，时间复杂度为O(n)
+    slow, fast = nums[0], nums[nums[0]]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+    fast = 0
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    return slow
--- 剑指Offer/MyCode/No_41.py
+++ 剑指Offer/MyCode/No_41.py
@@ -3,4 +3,26 @@
 leetcode 295. Find Median from Data Stream
 leetcode 480. Sliding Window Median
 '''
+import heapq
+
+class MedianFinder:
+# leetcode 295. Find Median from Data Stream
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.small = []     # 用最大堆存储数组中较小的一半数, small中所有数据小于large中的最小值
+        self.large = []     # 用最小堆存储数组中较大的一半数, large中所有数据大于small中的最大值
+
+    def addNum(self, num: int):
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+        else:
+            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+
+    def findMedian(self):
+        if len(self.small) == len(self.large):
+            return ( -self.small[0] + self.large[0] ) / 2     # 不要忘记self.small里存的是负数，取出时要加负号
+        else:
+            return self.large[0]
 
