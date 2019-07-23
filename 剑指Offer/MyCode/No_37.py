d1d5dc08aa6cf98499fce00318e41a9730bb2f0e
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-23 09:32:10 +0800

    add jianzhi 37_1

---
+++ 剑指Offer/MyCode/No_37.py
@@ -0,0 +1,41 @@
+'''
+剑指 Offer 第37题
+leetcode 297. Serialize and Deserialize Binary Tree
+leetcode 449. Serialize and Deserialize BST
+'''
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class SD_BT:
+    '''
+    二叉树的序列化和非序列化
+    '''
+    def serialize(root):
+        def helper(node):
+            if not node:
+                res.append("#")
+            else:
+                res.append(str(node.val))
+                helper(node.left)
+                helper(node.right)
+        res = []
+        helper(root)
+        return ",".join(res)
+
+    def deserialize(data):
+        def helper():
+            node_val = next(val_list)
+            if node_val == "#":
+                return None
+            else:
+                node = TreeNode(int(node_val))
+                node.left = helper()
+                node.right = helper()
+                return node
+
+        val_list = iter(data.split(","))
+        return helper()
