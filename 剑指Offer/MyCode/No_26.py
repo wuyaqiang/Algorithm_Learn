4c7917f00491c1b2d1fcdfe6abf08fdfa6578c7b
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-25 15:15:21 +0800

    add jianzhi 26_2

--- 剑指Offer/MyCode/No_26.py
+++ 剑指Offer/MyCode/No_26.py
@@ -5,14 +5,7 @@ leetcode 572. Subtree of Another Tree 树的子树
 注意：
 树的子结构和树的子树是两个不同的概念，
 即上面剑指 Offer 26 和 leetcode 572 题目不同。
-
 '''
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
 def is_sub_structure(s, t):
     '''
@@ -26,7 +19,6 @@ def is_sub_structure(s, t):
         if n1.val != n2.val:
             return False
         return check(n1.left, n2.left) and check(n1.right, n2.right)
-
     res = False
     if s and t:
         if s.val == t.val:
@@ -37,13 +29,36 @@ def is_sub_structure(s, t):
             is_sub_structure(s.right, t)
     return res
 
+
 def is_sub_tree(s, t):
     '''
     leetcode 572. Subtree of Another Tree 树的子树
     '''
+    # 解法一：类似剑指Offer的解法
+    # def check(n1, n2):
+    #     if n1 and n2:
+    #         return n1.val == n2.val and check(n1.left, n2.left) and check(n1.right, n2.right)
+    #     return True if not n1 and not n2 else False
+    # res = False
+    # if s and t:
+    #     if s.val == t.val:
+    #         res = check(s, t)
+    #     if not res:
+    #         res = is_sub_tree(s.left, t)
+    #     if not res:
+    #         res = is_sub_tree(s.right, t)
+    # return res
 
+    # 解法二：leetcode上解法，更简略
+    if not s:
+        return t == None
 
+    def check(n1, n2):
+        if n1 and n2:
+            return n1.val == n2.val and check(n1.left, n2.left) and check(n1.right, n2.right)
+        return True if not n1 and not n2 else False
 
+    return check(s, t) or is_sub_tree(s.left, t) or is_sub_tree(s.right, t)
 
 
 
