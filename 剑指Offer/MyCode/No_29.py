d025692de9d0636b0c12519056bbf149b1638864
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-25 11:15:50 +0800

    add jianzhi 29_2

--- 剑指Offer/MyCode/No_29.py
+++ 剑指Offer/MyCode/No_29.py
@@ -41,3 +41,60 @@ def generate_matrix(n):
     '''
     leetcode 59. Spiral Matrix II
     '''
+    if n < 1:
+        return []
+    res = [[0]*n for _ in range(n)]
+    nums = [i+1 for i in range(n*n)]
+
+    num_idx = 0
+    top, bottom, left, right = 0, n - 1, 0, n - 1
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            res[top][i] = nums[num_idx]
+            num_idx += 1
+        top += 1
+
+        for i in range(top, bottom + 1):
+            res[i][right] = nums[num_idx]
+            num_idx += 1
+        right -= 1
+
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                res[bottom][i] = nums[num_idx]
+                num_idx += 1
+            bottom -= 1
+
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                res[i][left] = nums[num_idx]
+                num_idx += 1
+            left += 1
+
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
