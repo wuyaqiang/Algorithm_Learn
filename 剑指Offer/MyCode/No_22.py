d3e6daf421541a4269e13fcf02a3fd6d11123cf6
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-25 20:40:33 +0800

    add jianzhi 22

---
+++ 剑指Offer/MyCode/No_22.py
@@ -0,0 +1,27 @@
+'''
+剑指 Offer 第22题 链表中倒数第k个结点
+leetcode 19. Remove Nth Node From End of List
+'''
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def remove_nth_from_end(head, n):
+    if not head or n < 1:
+        return head
+    sentinel = ListNode(0)
+    sentinel.next = head
+    slow, fast = sentinel, sentinel
+    for _ in range(n):
+        if not fast:    # 防止 n 大于链表长度
+            return None
+        fast = fast.next
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+    slow.next = slow.next.next
+    return sentinel.next
+
+
