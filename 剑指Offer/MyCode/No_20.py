130db93de655211dd2a42100a46418b371699719
wuyaqiang <wuyaqiang0202@outlook.com>
2019-07-29 09:59:35 +0800

    add jianzhi 20

--- 剑指Offer/MyCode/No_20.py
+++ 剑指Offer/MyCode/No_20.py
@@ -7,3 +7,28 @@ def is_valid_number(s):
     '''
     判断输入的字符串 s 是否表示一个有效的数字
     '''
+    s = s.strip()
+    has_point, has_e, has_number = False, False, False
+    for i, c in enumerate(s):
+        if "0" <= c <= "9":
+            has_number = True
+        elif c == ".":
+            if has_point or has_e:
+                return False
+            has_point = True
+        elif c == "e":
+            if has_e or not has_number:
+                return False
+            has_number = False
+            has_e = True
+        elif c == "+" or c == "-":
+            if i > 0 and s[i-1] != "e":
+                return False
+        else:
+            return False
+
+    return has_number
+
+
+
+
